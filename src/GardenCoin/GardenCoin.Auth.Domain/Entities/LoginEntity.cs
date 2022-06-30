0223195b8f5dba2326ab11cc61a15cfc099429cd
Franklin Bitencourt <you@example.com>
2022-06-30 17:03:30 -0300

    Adiciona Migration Inicial pro serviço de Autenticação

---
+++ src/GardenCoin/GardenCoin.Auth.Domain/Entities/LoginEntity.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GardenCoin.Auth.Domain.Entities
+{
+    [Table("TB_LOGIN")]
+    public class LoginEntity
+    {
+        [Column("EMAIL")]
+        public string Email { get; set; }
+
+        [Column("SENHA")]
+        public string Senha { get; set; }
+
+        [Column("ID_USUARIO")]
+        public Guid IdUsuario { get; set; }
+
+        public UsuarioEntity Usuario { get; set; }
+    }
+}
---
+++ src/GardenCoin/GardenCoin.Auth.Infra.Data/Contexts/GardenCoinAuthContext.cs
@@ -0,0 +1,34 @@
+﻿using GardenCoin.Auth.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GardenCoin.Auth.Infra.Data.Contexts
+{
+    public class GardenCoinAuthContext : DbContext
+    {
+        public GardenCoinAuthContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<LoginEntity>(entity =>{ 
+                entity
+                    .HasOne(l => l.Usuario)
+                    .WithMany(u => u.Logins)
+                    .HasForeignKey(x => x.IdUsuario)
+                    .IsRequired();
+
+                entity.HasKey(x => new{
+                    x.IdUsuario,
+                    x.Email
+                });
+            });
+                
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<LoginEntity> Login { get; set; }
+        public DbSet<UsuarioEntity> Usuarios { get; set; }
+    }
+}
